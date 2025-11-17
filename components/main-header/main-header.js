9b96d466fa52d7bafd9e07bfad8f6a5d45a97269
pulok1914 <pulok@buglelab.io>
2025-11-17 16:17:54 +0600

    homepage and community page added

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,11 +1,16 @@
 import Link from "next/link"
 import Image from "next/image"
+import MainHeaderBackground from "./main-header-background"
+import NavLink from "./nav-link"
 import classes from "./main-header.module.css"
 
 import logoImg from "@/assets/logo.png"
 
+
 export default function MainHeader(){
     return(
+        <>
+        <MainHeaderBackground />
         <header className={classes.header}>
             <Link className={classes.logo} href="/">
                 <Image src={logoImg} alt="A plate with food on it" priority />
@@ -15,13 +20,14 @@ export default function MainHeader(){
             <nav className={classes.nav}>
                 <ul>
                     <li>
-                        <Link href="/meals">Browse Meals</Link>
+                        <NavLink href="/meals">Browse Meals</NavLink>
                     </li>
                     <li>
-                        <Link href="/community">Foodie Community</Link>
+                        <NavLink href="/community">Join Community</NavLink>
                     </li>
                 </ul>
             </nav>
         </header>
+        </>
     )
 }
