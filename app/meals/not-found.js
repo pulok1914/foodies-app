4595fd67ef5e89ca9bdbca2b6e739aee8f0ebd80
pulok1914 <pulok@buglelab.io>
2025-11-19 14:04:59 +0600

    meal detials page added

---
+++ app/meals/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>The meal not found!</h1>
+        </main>
+    )
+}
---
+++ app/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>The page not found!</h1>
+        </main>
+    )
+}
