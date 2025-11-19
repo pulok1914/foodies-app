ea3b4bb64d8c9d544afeb26cb92e3b4c73e51d54
pulok1914 <pulok@buglelab.io>
2025-11-19 12:33:17 +0600

    error.js page added

---
+++ app/meals/loading.js
@@ -0,0 +1,7 @@
+import classes from "./loading.module.css"
+
+export default function LoadingMeals(){
+    return (
+        <p className={classes.loading}>Page is loading</p>
+    )
+}
--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -2,7 +2,6 @@ import MealItem from "./meal-item"
 import classes from "./meals-grid.module.css"
 
 export default function MealsGrid({meals}){
-    console.log("meals list",meals)
     return (
         <ul className={classes.meals}>
             {meals.map(meal=>(
